f720c1a66e5223d0b7484a6364d61c7010741d96
Linh Le <lkl007@ucsd.edu>
2021-02-25 13:31:43 -0800

    thirdpartyAPI lab

--- public/js/mapQuest.js
+++ public/js/mapQuest.js
@@ -3,10 +3,10 @@ function initMap() {
 	L.mapquest.key = 'LD33aNtOreJM5qEztypvH2jSHmpOFZYx'
 
 	var map = L.mapquest.map('map', {
-		center: [32.88, -117.236],
+		center: [32.8785203, -117.23808],
 		layers: L.mapquest.tileLayer('map'),
 		zoom: 12,
 		zoomControl: false
 	});
-	L.marker([32.88, -117.236]).addTo(map)
+	L.marker([32.8785203, -117.23808]).addTo(map)
 }
